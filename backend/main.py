1868c2f911b8d6a8d31f95cebc6d7661a25fb6ea
CamLundrigan <calundrigan@mun.ca>
2025-05-22 16:03:35 -0230

    Began adding sqlite3 for database management

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,33 @@
 from flask import Flask, jsonify, request
 import requests
+import sqlite3
+
 app = Flask(__name__)
+
+def get_db_connection():
+    #create or open jobs.db
+    conn = sqlite3.connect('jobs.db')
+
+    conn.row_factory = sqlite3.Row
+
+    return conn
+
+conn = sqlite3.connect('jobs.db')
+cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        company TEXT NOT NULL,
+        location TEXT,
+        apply_link TEXT,
+        status TEXT DEFAULT 'Saved',
+        deadline TEXT
+    );
+""")
+conn.commit()
+cur.close()
+conn.close()
 
 
 
